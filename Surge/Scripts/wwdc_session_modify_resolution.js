1b5bea3d94d4589d576c220c624104d78540d29b
0x1306a94 <0x1306a94@gmail.com>
2024-06-07 17:20:25 +0800

    更新WWDC脚本

--- Surge/Scripts/wwdc_session_modify_resolution.js
+++ Surge/Scripts/wwdc_session_modify_resolution.js
@@ -5,16 +5,24 @@ const resolutionPattern = /RESOLUTION=(\d+x\d+)/;
 let body = $response.body;
 const lines = body.split("\n");
 let modifyBody = "";
-for (const line of lines) {
+let len = lines.length;
+let index = 0;
+while (len > 0 && index < len) {
+  const line = lines[index];
   const match = line.match(resolutionPattern);
   if (match) {
     const resolution = match[1];
     if (keepResolutions.includes(resolution)) {
       modifyBody += line + "\n";
+    } else {
+      index += 2;
+      continue;
     }
   } else {
     modifyBody += line + "\n";
   }
+
+  index++;
 }
 
 let headers = $response.headers;
